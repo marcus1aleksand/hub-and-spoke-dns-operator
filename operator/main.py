89e7b15594dc31a5e37806eb70e631ba3d2bf536
Marcus Aleksandravicius <marcus1aleksand@gmail.com>
2024-08-14 20:34:09 -0400

    fix health check

--- operator/main.py
+++ operator/main.py
@@ -122,7 +122,7 @@ app.router.add_get("/readyz", readiness_check)
 
 
 async def main():
-    health_check_server = web._run_app(app, host="127.0.0.1", port=8080)
+    health_check_server = web._run_app(app, host="0.0.0.0", port=8080)
     kopf_operator = kopf.operator()
 
     await asyncio.gather(health_check_server, kopf_operator)
