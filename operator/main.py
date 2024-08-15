25a30d046bcd09a68993e1846bb76f14cd6e5b69
Marcus Aleksandravicius <marcus1aleksand@gmail.com>
2024-08-14 21:02:02 -0400

    skip bandit test for health check

--- operator/main.py
+++ operator/main.py
@@ -121,8 +121,8 @@ async def readiness_check(request):
 app.router.add_get("/readyz", readiness_check)
 
 
-async def main():  # nosec
-    health_check_server = web._run_app(app, host="0.0.0.0", port=8080)
+async def main():
+    health_check_server = web._run_app(app, host="0.0.0.0", port=8080)  # nosec
     kopf_operator = kopf.operator()
 
     await asyncio.gather(health_check_server, kopf_operator)
