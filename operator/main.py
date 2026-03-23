7c6ba1fb64b2ac2b8a8e4cf5de16c431dff0cb74
Mark (Dev Agent) <mark@agent.dev>
2026-03-23 10:02:52 -0400

    fix: align OPERATOR_VERSION default with chart version (0.4.3)

--- operator/main.py
+++ operator/main.py
@@ -92,7 +92,7 @@ dns_zone = (
 
 custom_ip_from_values = os.environ.get("CUSTOM_IP", None)
 
-OPERATOR_VERSION = os.environ.get("OPERATOR_VERSION", "0.4.2")
+OPERATOR_VERSION = os.environ.get("OPERATOR_VERSION", "0.4.3")
 operator_info.labels(
     dns_zone=dns_zone,
     provider=dns_provider.provider_name,
